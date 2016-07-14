b1c31385616b27438da301031e22e5cdb3be1bb3
Chyzwar <schizek.marcin@gmail.com>
2016-07-14 13:01:44 +0100

    versions

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import 'jquery';
+import 'bootstrap';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.scss';
+
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import AppComponent from './app.component.js';
 import Common from './common/common';
 import Components from './components/components';
-import './styles.scss';
+
 
 angular.module('myApp', [
   uiRouter,
